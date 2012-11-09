89de178dea809a35638c12e19278aa744a9b5c63
Laura Pesola <laura.pesola@helsinki.fi>
2012-11-09 15:17:47 +0200

    Tallennusnappi tallentaa tiedostoon sarakkeiden nimet

---
+++ Nerompi2/src/fi/helsinki/cs/nero/logic/ReportPrinter.java
@@ -0,0 +1,45 @@
+
+package fi.helsinki.cs.nero.logic;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.TableColumn;
+
+/**
+ *
+ * @author lpesola
+ */
+public class ReportPrinter {
+    
+    String fileName;
+    File file;
+    BufferedWriter writer;
+    
+    public ReportPrinter(String name) {
+        fileName = name;
+        file = new File(name);
+        try {
+            writer = new BufferedWriter(new FileWriter(file));
+        } catch (IOException ex) {
+            Logger.getLogger(ReportPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void print(Enumeration<TableColumn> columns) {
+        try {
+            while (columns.hasMoreElements()) {
+            writer.append(columns.nextElement().getHeaderValue().toString()); 
+            writer.newLine();
+            }
+            writer.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(ReportPrinter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
