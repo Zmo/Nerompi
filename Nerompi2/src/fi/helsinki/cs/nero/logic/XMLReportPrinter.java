017a645d7aa318f31d7529c317ec166a306a046e
Laura Pesola <laura.pesola@helsinki.fi>
2012-11-12 14:54:03 +0200

    XML-kirjoitin

---
+++ Nerompi2/src/fi/helsinki/cs/nero/logic/XMLReportPrinter.java
@@ -0,0 +1,17 @@
+package fi.helsinki.cs.nero.logic;
+
+import java.util.Enumeration;
+import javax.swing.table.TableColumn;
+
+/**
+ *
+ * @author lpesola
+ */
+public class XMLReportPrinter implements ReportWriter {
+
+    @Override
+    public void print(Object[][] tableData, Enumeration<TableColumn> columns) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
