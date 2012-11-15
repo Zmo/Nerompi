8f30a2ee8072a66d5cc4823389ca8c357b65d1d5
rkolagus <rkolagus@dcb-26.cs.helsinki.fi>
2012-11-15 12:08:22 +0200

    Kalenterinappimuutos 3.3.2 - päällekkäisyystarkistukset kehitteillä

--- Nerompi2/src/fi/helsinki/cs/nero/ui/Kalenterinappi.java
+++ Nerompi2/src/fi/helsinki/cs/nero/ui/Kalenterinappi.java
@@ -17,13 +17,14 @@ public class Kalenterinappi extends JCalendarButton {
     private static String[] kuulyhenteet = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     private TimelineElement element;
     private boolean onkoAlku;
-    
+
     public Kalenterinappi(Date dateTarget) {
         super(dateTarget);
         this.setIcon(null);
         this.asetaAikaTeksti();
     }
-    public Kalenterinappi(Date dateTarget, TimelineElement element, boolean onkoAlku){
+
+    public Kalenterinappi(Date dateTarget, TimelineElement element, boolean onkoAlku) {
         this(dateTarget);
         this.element = element;
         this.onkoAlku = onkoAlku;
@@ -32,52 +33,53 @@ public class Kalenterinappi extends JCalendarButton {
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         if (evt.getPropertyName().equalsIgnoreCase("date")) {
-            if (evt.getNewValue() == (this.getTargetDate())) {
-                System.out.println("Paiva ei muuttunut");
-            } else {
-                Date tulos = new Date();
-                tulos = this.getTargetDate();
-                try {
-                    tulos.setHours(12);
-                    tulos.setDate(Integer.parseInt(evt.getNewValue().toString().substring(8, 10)));
-                    int kuunumero = -1;
-                    for (int i = 0; i < kuulyhenteet.length && (kuunumero == -1); i++) {
-                        if (kuulyhenteet[i].equalsIgnoreCase(evt.getNewValue().toString().substring(4, 7))) {
-                            kuunumero = i;
+            Date tulos;
+            tulos = this.parseAika(evt);
+            if ((this.onkoAlku && this.element.onkoAlkuEnnenLoppua(tulos, this.element.getLoppukalenteri().getTargetDate()))
+                    || (!(this.onkoAlku) && this.element.onkoAlkuEnnenLoppua(this.element.getAlkukalenteri().getTargetDate(), tulos))) {
+                this.setTargetDate(tulos);
+                this.asetaAikaTeksti();
+                System.out.println("Muutettu kohde: " + this.getTargetDate() + "\n - - Paivavalinta muuttui - -");
+                
+                /* AIKATARKASTUKSIA - TYÖN ALLA*/
+                /*for (int a = 0; a < this.element.getReservation().getReservingPerson().getReservations().length; a++) {
+                    if (this.element.getReservation().getReservingPerson().getReservations()[a].getReservationID().equals(this.element.getReservation().getReservationID())) {
+                    } else {
+                        if (this.onkoAlku && this.element.getReservation().getReservingPerson().getReservations()[a].getTimeSlice().getEndDate().after(tulos)) {
+                            tulos = this.element.getReservation().getReservingPerson().getReservations()[a].getTimeSlice().getEndDate();
                         }
                     }
-                    tulos.setMonth(kuunumero);
-                    tulos.setYear(Integer.parseInt(evt.getNewValue().toString().substring(24, evt.getNewValue().toString().length())) - 1900);
-                } catch (Exception e) {
-                    System.out.println(" - VIRHE - Kalenterinappi.propertyChange: " + e);
-                }
-                if ((this.onkoAlku && this.element.updateNappiPaivays(tulos, element.getLoppukalenteri().getTargetDate())) ||
-                   (!(this.onkoAlku) && this.element.updateNappiPaivays(element.getAlkukalenteri().getTargetDate(), tulos))) {
-                    this.setTargetDate(tulos);
 
-                    System.out.println("Muutettu kohde: " + this.getTargetDate());
 
-                    this.asetaAikaTeksti();
-                    System.out.println(" - - Paivavalinta muuttui - -");
-                    /* VAARALLINEN MUUTOS */
-                    this.element.storeToDB();
-/* AIKATARKASTUKSIA - TYÖN ALLA*/
-/*                    for (int a = 0; a < this.element.getReservation().getReservingPerson().getReservations().length; a++){
-                        
-                        if (this.element.getReservation().getReservingPerson().getReservations()[a].getReservationID().equals(this.element.getReservation().getReservationID())){
-                        }
-                        else {
-                            if(this.onkoAlku && this.element.getReservation().getReservingPerson().getReservations()[a].getTimeSlice().getEndDate().after(tulos)){
-                                tulos = this.element.getReservation().getReservingPerson().getReservations()[a].getTimeSlice().getEndDate();
-                            }
-                        }
-                        
-                         
-                    }*/
-                    /* /AIKATARKASTUKSIA */
-                }
-/* / VAARALLINEN MUUTOS */
+                }*/
+                /* /AIKATARKASTUKSIA */
+                this.element.storeToDB();
             }
+            else {
+                System.out.println("VIRHE - TimelineElement - alkamispäivä yritetty siirtää loppumispäivän jälkeen.");
+                
+            }
+        }
+    }
+
+    private Date parseAika(PropertyChangeEvent evt) {
+        Date aika;
+        aika = this.getTargetDate();
+        try {
+            aika.setHours(12);
+            aika.setDate(Integer.parseInt(evt.getNewValue().toString().substring(8, 10)));
+            int kuunumero = -1;
+            for (int i = 0; i < kuulyhenteet.length && (kuunumero == -1); i++) {
+                if (kuulyhenteet[i].equalsIgnoreCase(evt.getNewValue().toString().substring(4, 7))) {
+                    kuunumero = i;
+                }
+            }
+            aika.setMonth(kuunumero);
+            aika.setYear(Integer.parseInt(evt.getNewValue().toString().substring(24, evt.getNewValue().toString().length())) - 1900);
+            return aika;
+        } catch (Exception e) {
+            System.out.println(" - VIRHE - Kalenterinappi.propertyChange: " + e);
+            return this.getTargetDate();
         }
     }
 
