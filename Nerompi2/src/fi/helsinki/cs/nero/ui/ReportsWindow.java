0f2e55a2020a8913c00ea6dbe609153b4b883cb0
Laura Pesola <laura.pesola@helsinki.fi>
2012-12-10 10:36:16 +0200

    lisätty valikko mappausten välillä vaihtamista varten

--- Nerompi2/src/fi/helsinki/cs/nero/ui/ReportsWindow.java
+++ Nerompi2/src/fi/helsinki/cs/nero/ui/ReportsWindow.java
@@ -150,6 +150,8 @@ public class ReportsWindow extends javax.swing.JFrame {
         showPostCount = new javax.swing.JCheckBox();
         showLocker = new javax.swing.JCheckBox();
         showWing = new javax.swing.JCheckBox();
+        dataModeDropdown = new javax.swing.JComboBox();
+        jLabel7 = new javax.swing.JLabel();
 
         fileChooserDialog.setDialogTitle("Tallenna");
 
@@ -488,6 +490,11 @@ public class ReportsWindow extends javax.swing.JFrame {
                     .addContainerGap(18, Short.MAX_VALUE)))
         );
 
+        dataModeDropdown.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Henkilöt", "Huoneet" }));
+        dataModeDropdown.setToolTipText("Näytetäänkö tiedot huoneiden vai henkilöiden mukaan");
+
+        jLabel7.setText("Näytä kaikki");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -501,7 +508,9 @@ public class ReportsWindow extends javax.swing.JFrame {
                             .addComponent(ColumnsLabel)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(6, 6, 6)
-                                .addComponent(columnChekboxes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(columnChekboxes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(dataModeDropdown, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel7))
                         .addGap(20, 20, 20)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -518,7 +527,11 @@ public class ReportsWindow extends javax.swing.JFrame {
                 .addComponent(restrictionsContainer, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(21, 21, 21)
+                        .addGap(9, 9, 9)
+                        .addComponent(jLabel7)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(dataModeDropdown, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
                         .addComponent(ColumnsLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(columnChekboxes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -730,6 +743,7 @@ public class ReportsWindow extends javax.swing.JFrame {
     private javax.swing.JLabel ColumnsLabel;
     private javax.swing.JTable Data;
     private javax.swing.JPanel columnChekboxes;
+    private javax.swing.JComboBox dataModeDropdown;
     private javax.swing.JFileChooser fileChooserDialog;
     private javax.swing.JComboBox fileTypeChooser;
     private net.sourceforge.jcalendarbutton.JCalendarButton firstCalendar;
@@ -741,6 +755,7 @@ public class ReportsWindow extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private net.sourceforge.jcalendarbutton.JCalendarButton lastCalendar;
     private javax.swing.JOptionPane overwriteCheck;
     private javax.swing.JLabel personLabel;
