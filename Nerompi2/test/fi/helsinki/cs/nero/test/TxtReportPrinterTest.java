d9d46e17a354c1b729529650b7f208e63973792c
Laura Pesola <laura.pesola@helsinki.fi>
2012-12-13 10:52:05 +0200

    txtreportprinterin testit valmiit & läpi

--- Nerompi2/test/fi/helsinki/cs/nero/test/TxtReportPrinterTest.java
+++ Nerompi2/test/fi/helsinki/cs/nero/test/TxtReportPrinterTest.java
@@ -1,4 +1,3 @@
-
 package fi.helsinki.cs.nero.test;
 
 import fi.helsinki.cs.nero.logic.TxtReportPrinter;
@@ -8,8 +7,6 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import junit.framework.TestCase;
 
 /**
@@ -17,21 +14,20 @@ import junit.framework.TestCase;
  * @author lpesola
  */
 public class TxtReportPrinterTest extends TestCase {
-    
+
     public TxtReportPrinterTest(String testName) {
         super(testName);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
-
 
     public void testPrintEmpty() {
         File f = new File("TxtReportPrinterTest.txt");
@@ -46,8 +42,8 @@ public class TxtReportPrinterTest extends TestCase {
         }
         assertEquals(expResult, result);
     }
-    
-        public void testPrintOneRow() {
+
+    public void testPrintOneRow() {
         try {
             File f = new File("TxtReportPrinterTest.txt");
             List<List> data = new ArrayList();
@@ -58,16 +54,16 @@ public class TxtReportPrinterTest extends TestCase {
             data.add(row);
             TxtReportPrinter instance = new TxtReportPrinter(f);
             instance.print(data);
-            
+
             String result = readFile(f);
-            String expResult = "eka| toka| kolmas| ";
+            String expResult = "eka| toka| kolmas| \n";
             assertEquals(expResult, result);
         } catch (FileNotFoundException ex) {
             fail();
         }
     }
-        
-        public void testPrint() {
+
+    public void testPrint() {
         try {
             File f = new File("TxtReportPrinterTest.txt");
             List<List> data = new ArrayList();
@@ -80,49 +76,53 @@ public class TxtReportPrinterTest extends TestCase {
             row2.add("eka2");
             row2.add("toka2");
             row2.add("kolmas2");
-            
+
             List<String> row3 = new ArrayList();
             row3.add("eka3");
             row3.add("toka3");
             row3.add("kolmas3");
-            
+
             data.add(row);
             data.add(row2);
             data.add(row3);
             TxtReportPrinter instance = new TxtReportPrinter(f);
             instance.print(data);
-            
+
             String result = readFile(f);
             String expResult = "eka | toka | kolmas | \n"
-                             + "eka2| toka2| kolmas2| \n"
-                             + "eka3| toka3| kolmas3| \n";
-            
-            System.out.println("expected: "+expResult);
-            System.out.println("result: "+result);
-                    
-            
+                    + "eka2| toka2| kolmas2| \n"
+                    + "eka3| toka3| kolmas3| \n";
+
+            System.out.println("expected: " + expResult);
+            System.out.println("result: " + result);
+
+
             assertEquals(expResult, result);
         } catch (FileNotFoundException ex) {
             fail();
         }
     }
-        
-        public void testPrintOneColumn() {
-        System.out.println("print");
-        List<List> data = null;
-        TxtReportPrinter instance = null;
-        instance.print(data);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-        
-        public void testEqualColumnWidths() {
-        System.out.println("print");
-        List<List> data = null;
-        TxtReportPrinter instance = null;
-        instance.print(data);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+    public void testPrintOneColumn() {
+        try {
+            File f = new File("TxtReportPrinterTest.txt");
+            List<List> data = new ArrayList();
+            List<String> row = new ArrayList();
+            row.add("vain yksi");
+            data.add(row);
+            TxtReportPrinter instance = new TxtReportPrinter(f);
+            instance.print(data);
+
+            String resultString = readFile(f);
+            String expResultString = "vain yksi| \n";
+            
+            boolean expResult = true;
+            boolean result = compare(resultString, expResultString);
+                       
+            assertEquals(expResult, result);
+        } catch (FileNotFoundException ex) {
+            fail();
+        }
     }
 
     private String readFile(File f) throws FileNotFoundException {
@@ -130,13 +130,34 @@ public class TxtReportPrinterTest extends TestCase {
         try {
             Scanner scanner = new Scanner(new FileReader(f));
             while (scanner.hasNextLine()) {
-                str = str.concat(scanner.nextLine()+"\n");                
+                str = str.concat(scanner.nextLine() + "\n");
             }
         } catch (FileNotFoundException ex) {
             throw ex;
         }
         return str;
     }
+    
+    private boolean compare(String result, String exp) {
+        boolean b = false;
         
-
+        if (result.length() == exp.length()) {
+            b = true;
+            for (int i = 0; i < result.length(); i++) {
+                System.out.print("result: "+result.charAt(i));
+                System.out.print(" exp: "+exp.charAt(i) + "\n");
+                if (result.charAt(i) != exp.charAt(i)) {
+                    b = false;
+                    break;
+                }
+            } 
+        } else {
+            System.out.println("eripituiset");
+            System.out.println("exp: "+exp.length());
+            System.out.println("result: "+result.length());
+        }
+        
+        
+        return b;
+    }
 }
