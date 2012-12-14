135434f4c660db642b7efa3d6410032fcccf6aad
Laura Pesola <laura.pesola@helsinki.fi>
2012-12-14 11:08:36 +0200

    reportsession kommentointia

--- Nerompi2/src/fi/helsinki/cs/nero/logic/ReportSession.java
+++ Nerompi2/src/fi/helsinki/cs/nero/logic/ReportSession.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package fi.helsinki.cs.nero.logic;
 
 import java.util.Vector;
 
 /**
- *
+ * Käsittelee Session tuottamaa dataa.
+ * 
  * @author lpesola
  */
 public interface ReportSession {
